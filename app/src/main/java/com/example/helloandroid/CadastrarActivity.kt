82e284f7b35d966b62aa094aba60fc99dcbfebdb
Janis Boiko <janisboikor@gmail.com>
2022-09-29 22:38:47 -0300

    "paginas novas"

---
+++ app/src/main/java/com/example/helloandroid/CadastrarActivity.kt
@@ -0,0 +1,13 @@
+package com.example.helloandroid
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CadastrarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastrar)
+    }
+
+}
--- app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.helloandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.helloandroid.R.id
+import com.example.helloandroid.R.id.*
+import com.google.android.ads.mediationtestsuite.activities.HomeActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(btLogin).SetOnClickListener{
+            startActivity(Intent(this.HomeActivity::class.java))
+        }
+        findViewById<Button>(btCadastrar).SetOnClickListener{
+            startActivity(Intent(this.CadastrarActivity::class.java))
+        }
+        findViewById<Button>(btEsqueciSenha).SetOnClickListener{
+            startActivity(Intent(this.EsqueciMinhaSenhaActivity::class.java))
+        }
     }
 }
