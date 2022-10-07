3101477671f7c24f694b8c68f2b230b98c014cd4
Janis Boiko <janisboikor@gmail.com>
2022-10-07 17:35:12 -0300

    tudo com lambda

--- app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -4,23 +4,43 @@ import android.content.Intent
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat.startActivity
 import com.example.helloandroid.R.id
 import com.example.helloandroid.R.id.*
 import com.google.android.ads.mediationtestsuite.activities.HomeActivity
+
+//public interface OnClickListener{
+//    void onClick(View view);
+//}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.btLogin).SetOnClickListener{
-            startActivity(Intent(this.HomeActivity::class.java))
+        //delega o tratamento para o metodo correto
+        findViewById<Button>(R.id.btLogin).setOnClickListener{
+            onClickLogin()
         }
-        findViewById<Button>(R.id.btCadastrar).SetOnClickListener{
-            startActivity(Intent(this.CadastrarActivity::class.java))
+        findViewById<Button>(R.id.btCadastrar).setOnClickListener {
+            onClickCadastrar()
         }
-        findViewById<Button>(R.id.btEsqueciSenha).SetOnClickListener{
-            startActivity(Intent(this.EsqueciMinhaSenhaActivity::class.java))
+        findViewById<Button>(R.id.btEsqueciSenha).setOnClickListener {
+           onClickEsqueciSenha()
         }
+        }
+
+    //um metodo para cada evento
+    private fun onClickLogin() {
+        startActivity(Intent(this,HomeActivity::class.java))
+    }
+
+    private fun onClickCadastrar() {
+        startActivity(Intent(this,CadastrarActivity::class.java))
+    }
+
+    private fun onClickEsqueciSenha() {
+        startActivity(Intent(this,EsqueciMinhaSenhaActivity::class.java))
     }
 }
